03bd45b59e71839e2f3dc668818029576cc8aa2d
tangjianfeng <patinousward@aliyun.com>
2019-11-25 16:58:24 +0800

    update

--- scala/pd-scalafunction/src/test/scala/com/patinousward/demo/scalafunction/FunctionTest01.scala
+++ scala/pd-scalafunction/src/test/scala/com/patinousward/demo/scalafunction/FunctionTest01.scala
@@ -2,6 +2,8 @@ package com.patinousward.demo.scalafunction
 
 import org.junit.Test
 
+import scala.annotation.tailrec
+
 /**
   * Created by Administrator on 2019/11/14.
   */
@@ -22,7 +24,9 @@ class FunctionTest01 {
     case class Person(depart:String,name:String)
     val a = List(Person("A部门","zhangsan"),Person("B部门","lisi"),Person("B部门","wangwu"))
     val by: Map[String, List[Person]] = a.groupBy(_.depart)//map的key是部门，类型是部门类型，value则是同一部门的Person的集合
+    // TODO: 另一种写法
     a.groupBy(_ depart)
+
   }
 
   @Test//zip unzip
@@ -42,10 +46,11 @@ class FunctionTest01 {
     }
     val a = List(A(18),A(16),A(24),A(35))
     val reduce: A = a.reduce((a1,a2)=>a1.combine(a2))//返回得出的是数组中的类型
+    // TODO:  另一种写法
     val reduce2: A = a.reduce(_ combine _)//返回得出的是数组中的类型
     print(reduce)//A(93)
   }
-  @Test//factorial 阶乘 斐波那契数
+  @Test//练习2.1factorial 阶乘 斐波那契数
   def test05():Unit={
     def fib(n: Int): Int = {
       @annotation.tailrec
@@ -56,5 +61,64 @@ class FunctionTest01 {
     }
     print(fib(6))
   }
+
+  @Test//练习2.2
+  def test06(): Unit ={
+    def isSorted[A](as:Array[A],ordered:(A,A)=>Boolean):Boolean={
+      @tailrec
+      def loop(n:Int):Boolean ={
+        if(n >=as.length -1) true
+        else if(!ordered(as(n),as(n+1))) false
+        else loop(n +1)
+      }
+      loop(0)
+    }
+
+    print(isSorted(Array(1,2,3,8,5),(a1:Int,a2:Int)=>a1< a2))
+  }
+
+  @Test
+  def test07(): Unit ={
+    def partiall[A,B,C](a:A,f:(A,B)=>C):B=>C={
+      b:B =>f(a,b)
+        // TODO:
+      //e:B =>f(a,e)  小b只是B传入参数的一种代表，a变量固定死了，B是可以任意的
+        //分析这个方法的入参可以知道，传入的只有一个参数a和函数f，通过分析这个方法的返回值知道
+        //返回值是一个传入B类型，返回C的函数，就是说，这个返回函数调用的时候，必然会传入参数B，
+        //所以这个方法能使用的参数其实有2个，一个是a，这个不能修改，一个是返回函数以后会调用的B，用b代表这个参数，当然也可以使用别的代替
+    }
+
+    def cc(a:Int,f:(Int,String)=>Boolean):String=>Boolean ={
+      b:String =>f(a,b)
+    }
+
+    val stringToBoolean = cc(100,(a:Int,b:String)=>b.toInt == a)
+    print(stringToBoolean("100"))
+  }
+
+  @Test//练习2.3 柯里化
+  def test08(): Unit ={
+    def curry[A,B,C](f:(A,B)=>C):A =>(B =>C)={
+      a:A => b:B =>f(a,b)
+    }
+    // a代表返回函数将来调用时的入参数，b代表返回函数的返回函数将来调用时的入参,但是这个b参数只能在a的返回参数中调用
+    //就是说，要在a:A => 后面才能使用到这个参数，否则变量的范围是有问题的
+  }
+  @Test//练习2.4  反柯里化
+  def test09(): Unit ={
+    def uncurry[A,B,C](f:A=>B=>C):(A,B)=>C={
+      (a:A,b:B) =>f(a)(b)
+        //f(a)  返回B =>C  这时候再使用()，传入b进行调用即可
+    }
+  }
+
+  @Test//练习2.5
+  def test10(): Unit ={
+    def compose[A,B,C](f:B=>C,g:A=>B):A =>C ={
+      a:A =>f(g(a))
+    }
+  }
+
+
 
 }
