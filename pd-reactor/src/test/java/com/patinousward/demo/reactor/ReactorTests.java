1965244af39818f3a43e934dcd5926bc5607dbff
tangjianfeng <patinousward@aliyun.com>
2019-12-27 01:15:28 +0800

    add reactor module

---
+++ pd-reactor/src/test/java/com/patinousward/demo/reactor/ReactorTests.java
@@ -0,0 +1,48 @@
+package com.patinousward.demo.reactor;
+
+import org.junit.After;
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.lang.Thread.sleep;
+
+public class ReactorTests {
+        @After
+    public void after() throws InterruptedException {
+        sleep(30_000);
+    }
+
+    @Test
+    public void testJust() {
+        Flux.just("hello", "world")
+                .subscribe(System.out::println);
+    }
+
+    @Test
+    public void testList() {
+        List<String> words = Arrays.asList(
+                "hello",
+                "reactive",
+                "world"
+        );
+
+        Flux.fromIterable(words)
+                .subscribe(System.out::println);
+    }
+
+    @Test
+    public void testRange() {
+        Flux.range(1, 10)
+                .subscribe(System.out::println);
+    }
+
+    @Test
+    public void testInterval() {
+        Flux.interval(Duration.ofSeconds(1))
+                .subscribe(System.out::println);
+    }
+}
